4299c3b278ed2178d1ac14c059effd36182664da
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2022-11-15 18:36:57 -0600

    Time: 3 ms (56.61%), Space: 7.2 MB (29.40%) - LeetHub

---
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) {
+        unordered_set<float> s;
+        int L = 0, R = nums.size()-1;
+        
+        sort(nums.begin(), nums.end());
+        while(L < R)
+        {
+            float avg = ((float)nums[L++] + (float)nums[R--])/2;
+            s.insert(avg);
+        }
+        
+        return s.size();
+        
+        //Time: O(n log n)
+        //Space: O(n)
+    }
+};
