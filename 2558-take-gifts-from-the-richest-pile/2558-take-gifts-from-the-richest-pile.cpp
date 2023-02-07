5d105723a50a7151f8de5cbc54e750d7e57e7f76
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-02-06 18:12:30 -0600

    Time: 4 ms (94.17%), Space: 9.4 MB (39.82%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        // U : Pick highest number and replace it with sqrt;
+        // M : Priority queue;
+        // P : Add all elements to priority queue, pop the queue and add sqrt of front number to the queue, add up rest of queue at end after k secs;
+        // I :
+        priority_queue<long long> q;
+        long long res = 0;
+        
+        for(auto& n : gifts)
+        {
+            q.push(n);
+        }
+        
+        for(int i = 0; i < k; i++)
+        {
+            double root = q.top();
+            q.pop();
+            q.push(floor(sqrt(root))); 
+        }
+        
+        while(!q.empty())
+        {
+            res += q.top();
+            q.pop();
+        }
+        
+        return res;
+        
+        // R : Looks good
+        // E : t (n log n) s (n)
+    }
+};
