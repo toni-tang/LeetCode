14eac7e2364e4ab1f9daf96d9a2ca488db593fe5
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-02-07 13:10:52 -0600

    Time: 173 ms (80.03%), Space: 71.3 MB (48.83%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& tree) {
+    int totalFruit(vector<int>& fruits) {
         unordered_map<int, int> count;
         int i, j;
-        for (i = 0, j = 0; j < tree.size(); ++j) {
-            count[tree[j]]++;
+        for (i = 0, j = 0; j < fruits.size(); ++j) {
+            count[fruits[j]]++;
             if (count.size() > 2) {
-                if (--count[tree[i]] == 0)count.erase(tree[i]);
+                if (--count[fruits[i]] == 0)count.erase(fruits[i]);
                 i++;
             }
         }
