2bad8292a9f923e624c18d8c58e58b2938d1c213
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-02-15 18:11:20 -0600

    Time: 12 ms (48.69%), Space: 18.8 MB (63.94%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,7 +13,6 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(!root) return 0;
-        
         return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
