931e1194bd08f821a46ff50431d46b92e44c4416
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-02-16 12:43:43 -0600

    Time: 0 ms (100.00%), Space: 6.4 MB (62.05%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -5,11 +5,9 @@ public:
         unordered_map<string, int> mp2;
         stringstream ss(s);
         string word;
-        int count = 0;
         
         for(int i = 0; i < pattern.length(); i++) {
             ss >> word;
-            count++;
             
             if(mp.find(pattern[i]) == mp.end())
             {
@@ -22,12 +20,6 @@ public:
         }
         
         if((ss >> word)) return false;
-//         while(ss >> word)
-//         {
-//             count++;
-//         }
-        
-//         if(count != pattern.length()) return false;
         
         return true;
     }
