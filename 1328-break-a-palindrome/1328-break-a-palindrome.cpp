77ca26e1a667005b1765a827b67e4acdf228cc13
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-02-17 20:40:32 -0600

    Time: 4 ms (33.29%), Space: 6.1 MB (81.90%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,19 +3,15 @@ public:
     string breakPalindrome(string palindrome) {
         if(palindrome.length() < 2) return "";
         
-        for(int i = 0; i < palindrome.length(); i++) {
-            if(i == palindrome.length()/2 && palindrome.length()-1 != palindrome.length()/2);
-            else if(i == palindrome.length()-1 && palindrome[i] == 'a')
-            {
-                palindrome[i] = (int)palindrome[i] + 1;
-                return palindrome;
-            }
-            else if(palindrome[i] != 'a') 
+        for(int i = 0; i < palindrome.length()/2; i++) {
+            if(palindrome[i] != 'a') 
             {
                 palindrome[i] = 'a';
                 return palindrome;
             }
         }
+        
+        palindrome[palindrome.length()-1] = 'b';
         
         return palindrome;
     }
