fc0fde23d9322f3b5abec672fe6a2ce9fe247545
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-02-18 14:44:34 -0600

    Time: 40 ms (44.10%), Space: 13.4 MB (46.67%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int A = 0, B = 0;
+        int aMax = 0, bMax = 0;
+        
+        
+        int L = 0, R = 0;
+        for(int i = 0; i < colors.length(); i++)
+        {
+            if(colors[i] == 'A')
+            {
+                A++;
+            }
+            else
+            {
+                if(A >= 3)
+                {
+                    aMax += A - 2;
+                }
+                A = 0;
+            }
+        }
+        
+        for(int i = 0; i < colors.length(); i++)
+        {
+            if(colors[i] == 'B')
+            {
+                B++;
+            }
+            else
+            {
+                if(B >= 3)
+                {
+                    bMax += B - 2;
+                }
+                B = 0;
+            }
+        }
+        
+        if(A >= 3)
+        {
+            aMax += A - 2;
+        }
+        
+        if(B >= 3)
+        {
+            bMax += B - 2;
+        }
+        
+        cout << aMax << " " << bMax;
+        
+        return aMax > bMax ? true : false;
+    }
+};
