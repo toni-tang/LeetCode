b32b0e3b994633f7fb33e87a60ca9a6048cf4777
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-05-28 20:53:39 -0500

    Time: 636 ms (23.42%), Space: 67.1 MB (13.90%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s;
         unordered_map<int, int> mp;
         int res = 0;
         
@@ -10,19 +9,14 @@ public:
             mp[n]++;
         }
         
+        int i;
         for(auto n : nums) 
         {
             if(mp.find(n-1) == mp.end())
             {
-                s.insert(n);
+                for(i = n; mp.find(i) != mp.end(); i++);
+                res = max(res, i - n);
             }
-        }
-        
-        int i;
-        for(auto n : s)
-        {
-            for(i = n; mp.find(i) != mp.end(); i++);
-            res = max(res, i - n);
         }
         
         return res;
