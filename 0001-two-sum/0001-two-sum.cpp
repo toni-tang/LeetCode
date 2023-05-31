32c701ecd0eeb76a037030ef667dcef2f4a315f8
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-05-31 08:11:41 -0500

    Time: 13 ms (71.05%), Space: 10.8 MB (45.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,11 +5,11 @@ public:
         
         for(int i = 0; i < nums.size(); i++) 
         {
-            int res = target - nums[i];
+            int numToFind = target - nums[i];
     
-            if(mp.find(res) != mp.end())
+            if(mp.find(numToFind) != mp.end())
             {
-                return { mp[res], i };
+                return { mp[numToFind], i };
             }
             
             mp[nums[i]] = i;
