d0bcb3bb8332bea82eaa483e660f73bf61da58b6
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-07-01 11:48:07 -0500

    Time: 5 ms (33.53%), Space: 7.4 MB (57.33%) - LeetHub

---
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int res = 0;
+        int sum = 0;
+        
+        for(const auto& n : nums) {
+            sum += n;
+            res = min(sum, res);
+        }
+        
+        return res < 0 ? (-res) + 1 : 1;
+    }
+};
