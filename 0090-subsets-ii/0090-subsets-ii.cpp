44f79752134ee9b972ed37a4a2a0b8d2d0d54156
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-07-16 13:51:48 -0500

    Time: 3 ms (69.63%), Space: 7.4 MB (91.78%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        sort(nums.begin(), nums.end());
+        backtrack(res, nums, temp, 0);
+        return res;
+    }
+private:
+    void backtrack(vector<vector<int>>& res, vector<int>& nums, vector<int>& temp, int start)  {
+        res.push_back(temp);
+        
+        for(int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            temp.push_back(nums[i]);
+            backtrack(res, nums, temp, i+1);
+            temp.pop_back();
+        }
+    }
+};
