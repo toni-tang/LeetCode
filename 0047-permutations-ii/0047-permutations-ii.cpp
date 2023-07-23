f6377daa6e97998e6cb81841241a320cd4af9b2f
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-07-23 14:03:37 -0500

    Time: 9 ms (70.82%), Space: 8.3 MB (98.53%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> res;
+        
+        sort(nums.begin(), nums.end());
+        backtrack(res, nums, 0);
+        
+        return res;
+    }
+
+private: 
+    void backtrack(vector<vector<int>>& res, vector<int>& nums, int start) {
+        if(start >= nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+        
+        for(int i = start; i < nums.size(); ++i) {
+            if(i > start && nums[i] == nums[start]) {
+                continue;
+            }
+            swap(nums[i], nums[start]);
+            backtrack(res, nums, start+1);
+        }
+        
+        for (int i = nums.size() - 1; i > start; --i) {
+            swap(nums[i], nums[start]);
+        }
+    }
+};
