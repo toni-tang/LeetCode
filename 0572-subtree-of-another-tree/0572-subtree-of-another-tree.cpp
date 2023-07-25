3c58acef47115ab21130ab673be625bae3bdc47c
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-07-24 22:30:01 -0500

    Time: 20 ms (91.07%), Space: 28.8 MB (30.17%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -14,16 +14,11 @@ public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if(!root) return false;
         if(isIdentical(root, subRoot)) return true;
-        
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
     
     bool isIdentical(TreeNode* s, TreeNode* t) {
-        if(!s || !t)
-        {
-            return !s && !t;
-        }
-        
+        if(!s || !t) return !s && !t;
         return s->val == t->val && isIdentical(s->left, t->left) && isIdentical(s->right, t->right);
     }
 };
