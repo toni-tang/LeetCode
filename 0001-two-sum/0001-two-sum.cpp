8e657adb06744c9398499ab19764e0c842c687ea
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-08-15 13:28:22 -0500

    Time: 7 ms (94.93%), Space: 10.9 MB (23.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,13 +4,18 @@ public:
         unordered_map<int, int> mp;
         
         for(int i = 0; i < nums.size(); i++) {
-            mp[nums[i]] = i;
-        }
-        
-        for(int i = 0; i < nums.size(); i++) {
             int res = target - nums[i];
-            if(mp.find(res) != mp.end()) {
-                if(mp.at(res) != i) return { mp.at(res), i };
+            
+            for(int i = 0; i < nums.size(); i++) 
+            {
+                int numToFind = target - nums[i];
+
+                if(mp.find(numToFind) != mp.end())
+                {
+                    return { mp[numToFind], i };
+                }
+
+                mp[nums[i]] = i;
             }
         }
         
