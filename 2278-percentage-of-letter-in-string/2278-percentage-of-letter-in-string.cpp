11bbade76190184c45dc8c73a0169440b06bfb6b
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-08-18 14:49:01 -0500

    Time: 0 ms (100.00%), Space: 6.1 MB (47.00%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int count = 0;
+        for(auto c : s) {
+            if (c == letter) count++;
+        }
+        
+        return count*100/s.size();
+    }
+};
