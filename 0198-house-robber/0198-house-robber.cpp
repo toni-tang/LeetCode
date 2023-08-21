95218334a2ec03268a8a6445b452b5a84adc2c0d
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-08-21 10:52:11 -0500

    Time: 3 ms (37.67%), Space: 7.9 MB (17.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // UNDERSTAND 
-        // Objective: Max amount of money to rob
-        // Condition: Can't rob adjacent houses,
-        // Questions: Is the array ever empty?, Do houses have negative money??
+        vector<int> dp(nums.size()+1);
         
-        // MATCH
-        // Dynamic Programming - Fibonacci Sequence Pattern, 1D
-        
-        // PLAN
-        // Loop through array
-        // Compare sum[i] = max(arr[i-2] + a[i], arr[i-1]);
-        
-        
-        // IMPLEMENT
-        int A = 0, B = 0;
-        for(int i = 0; i < nums.size(); i++)
-        {
-            int temp = max(nums[i] + A, B);
-            A = B;
-            B = temp;
+        dp[1] = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            dp[i+1] = max(dp[i-1] + nums[i], dp[i]);    
         }
         
-        return B;
+        for(int n : dp) {
+            cout << n << " ";
+        }
         
-        // REVIEW
-        // EVALUATE 
+        cout << endl;
+        
+        return dp[nums.size()];
     }
 };
