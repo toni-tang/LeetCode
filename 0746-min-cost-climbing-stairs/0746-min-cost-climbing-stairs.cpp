603c5649594a07bad3c234668f778585af240068
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-08-21 12:08:42 -0500

    Time: 3 ms (91.01%), Space: 13.7 MB (72.00%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -5,12 +5,8 @@ public:
         if(n == 0) return 0;
         if(n == 1) return cost[0];
         if(n == 2) return min(cost[0], cost[1]);
-        
-        vector<int> dp(cost.size());
-        
-        int a = cost[0];
-        int b = cost[1];
-        int c;
+
+        int a = cost[0], b = cost[1], c = 0;
         for(int i = 2; i < cost.size(); i++) {
             c = min(a, b) + cost[i];   
             a = b;
