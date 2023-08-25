805589d315399e4099878da8ef55085acbc2c6f5
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-08-25 11:48:01 -0500

    Time: 36 ms (59.88%), Space: 19.1 MB (31.47%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> res;
         sort(intervals.begin(), intervals.end());
         
+        vector<vector<int>> res;
         res.push_back(intervals[0]);
+        
         for(int i = 0; i < intervals.size(); i++) {
-            if(res.back()[1] >= intervals[i][0]) res.back()[1] = max(res.back()[1], intervals[i][1]);
+            if(res.back()[1] >= intervals[i][0]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } 
             else {
                 res.push_back(intervals[i]);
             }
