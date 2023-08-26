ef3741050556cecb14ff1d05953c8b06ceae492d
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-08-25 20:10:28 -0500

    Time: 45 ms (86.59%), Space: 48.4 MB (42.79%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int reachable = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (i > reachable) {
-                return false;
-            }
-            
-            reachable = max(reachable, i + nums[i]);
-            
-            if (reachable >= n-1) {
-                break;
-            }
+        int reach = 0;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > reach) return false;
+            reach = max(reach, i + nums[i]);
+            if(reach >= n-1) break; 
         }
-
-        return true;
+        
+        return true;        
     }
 };
