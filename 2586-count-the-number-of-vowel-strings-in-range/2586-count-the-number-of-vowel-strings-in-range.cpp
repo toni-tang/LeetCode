6ba6c694b8fad3321c069ccea05bd627c9da4d4f
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-08 18:22:58 -0500

    Time: 15 ms (60.80%), Space: 31.7 MB (70.68%) - LeetHub

---
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int vowelStrings(vector<string>& words, int left, int right) {
+        int res = 0;
+        unordered_map<string, int> mp;
+        for(int i = left; i <= right; i++) {
+            string word = words[i];
+            int n = word.size() - 1;
+            if((word[0] == 'a' || word[0] == 'e' || word[0] == 'i' || word[0] == 'o' || word[0] == 'u') && (word[n] == 'a' || word[n] == 'e' || word[n] == 'i' || word[n] == 'o' || word[n] == 'u')) res++; 
+        }
+        
+        return res;
+    }
+};
