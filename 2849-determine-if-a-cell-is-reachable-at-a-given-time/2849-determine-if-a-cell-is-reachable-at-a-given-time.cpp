209441c6528f0ad09515285667aaad8b81d11647
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-10 15:39:36 -0500

    Time: 5 ms (33.33%), Space: 5.9 MB (33.33%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,38 +1,8 @@
 class Solution {
 public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int x = abs(abs(fx) - abs(sx));
-        int y = abs(abs(fy) - abs(sy));
-        
-        if(sx == fx && sy == fy && t == 1) return false;
-        if(sx == fx && sy == fy && t == 0) return true;
-        if((sx != fx || sy != fy) && t == 0) return false;
-        
-        if(x == y) {
-            t -= x;
-            x = 0;
-            y = 0;
-        }
-        else if(x != 0 && y != 0) {
-            int w = max(x,y);
-            int m = min(x,y);
-            
-            int z = m % w;
-            cout << z << endl;
-            t -= z;
-            x -= z;
-            y -= z;
-        }
-        
-        if(x > 0)   {  
-            t -= x;
-        }
-
-         if(y > 0)   {  
-            t -= y;
-        }
-        
-        
-        return t >= 0;
+        int xdiff = abs(sx - fx), ydiff = abs(sy - fy);
+        if(xdiff == 0 && ydiff == 0 && t == 1) return false;
+        return (min(xdiff, ydiff) + abs(xdiff - ydiff)) <= t;
     }
 };
