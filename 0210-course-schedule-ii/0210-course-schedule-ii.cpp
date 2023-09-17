7f27452d9c5c9c83c4719f7e394e5e5f85c4c5f5
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-17 00:48:35 -0500

    Time: 20 ms (45.24%), Space: 13.9 MB (56.69%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(n, vector<int>());
+        vector<int> degree(n, 0);
+        vector<int> res(n);
+        
+        for(auto& p : pre) {
+            adj[p[0]].push_back(p[1]);
+            degree[p[1]]++;
+        }
+        
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            if(degree[i] == 0) q.push(i);
+        }
+        
+        while(!q.empty()) {
+            int curr = q.front(); q.pop(); res[n-1] = curr; n--;
+            for(auto& node : adj[curr]) {
+                if (--degree[node] == 0) q.push(node); 
+            }
+        }
+        
+        return n == 0 ? res : vector<int>();
+    }
+};
