fd7d955ec1eb2d76a0351e13cfde562a6fd09558
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-22 10:30:50 -0500

    Time: 820 ms (33.72%), Space: 8.3 MB (20.34%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        bool res = false;
+    
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == word[0]) {
+                    res = max(res, dfs(board, word, i, j, 0));
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int idx) {
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || idx == word.size() || board[i][j] != word[idx]) {
+            return false;
+        }
+        
+        if(idx == word.size()-1) return true;
+        
+        char temp = board[i][j];
+        board[i][j] = '@';
+        
+        bool res = dfs(board, word, i+1, j, idx+1) || dfs(board, word, i-1, j, idx+1) || dfs(board, word, i, j+1, idx+1) || dfs(board, word, i, j-1, idx+1);
+        
+        board[i][j] = temp;
+        
+        return res;
+    }
+};
