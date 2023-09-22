7fc9f7180769c20f36defc71e36431ef17137558
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-22 11:51:44 -0500

    Time: 791 ms (43.04%), Space: 8.5 MB (7.38%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -8,12 +8,12 @@ public:
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(board[i][j] == word[0]) {
-                    res = max(res, dfs(board, word, i, j, 0));
+                    if (dfs(board, word, i, j, 0)) return true;
                 }
             }
         }
         
-        return res;
+        return false;
     }
     
     bool dfs(vector<vector<char>>& board, string word, int i, int j, int idx) {
