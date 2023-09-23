446bd448b834db6ae3d3b33ffce0015acb0fb007
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-22 21:13:13 -0500

    Time: 3 ms (98.14%), Space: 9.9 MB (75.98%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -3,22 +3,20 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         
-        for(int i = 1; i < m+1; i++) {
-            for(int j = 1; j < n+1; j++) {
-                if(i == 1) {
-                     dp[i][j] = dp[i][j-1] + grid[i-1][j-1];
-                }
-                else if(j == 1) {
-                    dp[i][j] = dp[i-1][j] + grid[i-1][j-1];
-                }
-                else {
-                    dp[i][j] = min(dp[i-1][j] + grid[i-1][j-1], dp[i][j-1] + grid[i-1][j-1]);   
-                }
+        vector<int> dp(m, grid[0][0]);
+        
+        for (int i = 1; i < m; i++) {
+            dp[i] = dp[i - 1] + grid[i][0];    
+        }
+        
+        for (int j = 1; j < n; j++) {
+            dp[0] += grid[0][j]; 
+            for (int i = 1; i < m; i++) {
+                dp[i] = min(dp[i - 1], dp[i]) + grid[i][j];   
             }
         }
         
-        return dp[m][n];
+        return dp[m - 1];
     }
 };
