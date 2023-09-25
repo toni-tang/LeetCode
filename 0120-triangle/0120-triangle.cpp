4a31e2fa0fb73a71ea6072c7d40a53923530a902
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-24 21:38:15 -0500

    Time: 4 ms (75.53%), Space: 8.9 MB (62.03%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-//     int minimumTotal(vector<vector<int>>& tri) {
-//         int res = INT_MAX;
-//         dfs(tri, 0, 0, res, 0);
-//         return res;
-//     }
-    
-//     void dfs(vector<vector<int>> grid, int i, int j, int& res, int sum) {
-//         if(i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size()) {
-//             return;
-//         }
-        
-//         if(i == grid.size()-1) {
-//             res = min(res, sum + grid[i][j]);
-//         }
-        
-//         dfs(grid, i+1, j, res, sum + grid[i][j]);
-//         dfs(grid, i+1, j+1, res, sum + grid[i][j]);
-//     }
-    
     int minimumTotal(vector<vector<int>>& tri) {
         int m = tri.size();
         for(int i = 1; i < m; i++) {
