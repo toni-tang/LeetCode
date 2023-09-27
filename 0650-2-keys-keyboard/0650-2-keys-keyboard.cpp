57b4acb95f2057ba134f3d0d2210a325cb3cbae3
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-27 14:44:45 -0500

    Time: 39 ms (14.14%), Space: 156 MB (7.56%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -7,14 +7,8 @@ public:
     }
     
     int dfs(int step, int val, int copy, int n, vector<vector<int>>& dp) {        
-        if(step > n || val > n) {
-            return INT_MAX;
-        }
-        
-        if(n == val) {
-            return step;
-        }
-    
+        if(step > n || val > n) return INT_MAX;
+        if(n == val) return step;
         if(dp[step][val] != -1) return dp[step][val];
     
         return dp[step][val] = min(dfs(step+1, val+copy, copy, n, dp), dfs(step+2, 2*val, val, n, dp));
