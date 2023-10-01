5940670dbcc8bd89f5e9d64cf71460121f2dae7b
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-09-30 23:49:50 -0500

    Time: 0 ms (100.00%), Space: 6.2 MB (37.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,14 +3,13 @@ public:
     int climbStairs(int n) {
         if(n <= 3) return n;
         
-        int a = 1, b = 2, c = 0;
-        n -= 2;
-        while(n--) {
+        int a = 1, b = 2, c;
+        while(n-- != 2) {
             c = a + b;
             a = b;
             b = c;
         }
-        //1, 2, 3, 5, 8
+        
         return c;
     }
 };
