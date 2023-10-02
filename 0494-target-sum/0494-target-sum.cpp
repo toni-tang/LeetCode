f0e2751be778847bedd5aca2e1ebd43092701997
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-10-02 16:17:25 -0500

    Time: 247 ms (41.56%), Space: 56.4 MB (18.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int res = 0;
-        dfs(nums, target, 0, 0, res);
-        return res;
+        map<pair<int, int>, int> dp;
+        return dfs(nums, target, 0, 0, dp);
     }
     
-    void dfs(vector<int>& nums, int target, int i, int sum, int& res) {
-        if(i >= nums.size()) return;
+    int dfs(vector<int>& nums, int target, int i, int sum, map<pair<int, int>, int>& dp) {
         if(i == nums.size()-1) {
-            if(sum - nums[i] == target) {
-                res++;
-            }
-            if(sum + nums[i] == target) {
-                res++;
-            }
-            return;
+            int res = 0;
+            
+            if(sum - nums[i] == target) res++;
+            if(sum + nums[i] == target) res++;
+            
+            return res;
         } 
         
-        dfs(nums, target, i+1, sum + nums[i], res);
-        dfs(nums, target, i+1, sum - nums[i], res);
+        if(dp.find({i, sum}) != dp.end()) return dp[{i, sum}];
+        
+        return dp[{i, sum}] = dfs(nums, target, i+1, sum + nums[i], dp) + dfs(nums, target, i+1, sum - nums[i], dp);
     }
 };
