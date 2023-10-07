ff78f79faf2d87059dfd6b2e6ecbc8394875a3f4
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-10-06 21:59:40 -0500

    Time: 30 ms (59.66%), Space: 19.3 MB (45.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,13 +3,14 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         
+        int n = intervals.size();
         vector<vector<int>> res;
         res.push_back(intervals[0]);
         
-        for(int i = 0; i < intervals.size(); i++) {
+        for(int i = 0; i < n; i++) {
             if(res.back()[1] >= intervals[i][0]) {
                 res.back()[1] = max(res.back()[1], intervals[i][1]);
-            } 
+            }
             else {
                 res.push_back(intervals[i]);
             }
