8257205cb7d7bde0616a1248cf33f45b29de1363
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-10-07 11:22:53 -0500

    Time: 396 ms (21.48%), Space: 90.2 MB (35.99%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -4,14 +4,14 @@ public:
         sort(intervals.begin(), intervals.end());
         
         int res = 0;
-        vector<int> prev = intervals[0];
+        int prev = intervals[0][1];
         
         for(int i = 1; i < intervals.size(); i++) {
-            if(prev[1] > intervals[i][0]) {
-                prev[1] = min(prev[1], intervals[i][1]);
+            if(prev > intervals[i][0]) {
+                prev = min(prev, intervals[i][1]);
                 res++;
             } else {
-                prev = intervals[i];
+                prev = intervals[i][1];
             }
         }
         
