1cbdc0ab9bb790640d0c9a455d305c00fc11324f
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-10-27 14:43:49 -0500

    Time: 82 ms (45.03%), Space: 14.9 MB (60.60%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
         vector<int> dp(amount+1, amount+1);
+        
         dp[0] = 0;
         
-        for(int i = 1; i < amount+1; i++) {
+        for(int i = 0; i < amount+1; i++) {
             for(int j = 0; j < coins.size(); j++) {
                 if(i - coins[j] >= 0) {
-                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
-                    
-                }
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                }   
             }
         }
         
-        if(dp[amount] == amount + 1) {
-            return -1;
-        }
-        
-        return dp[amount];
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
 };
