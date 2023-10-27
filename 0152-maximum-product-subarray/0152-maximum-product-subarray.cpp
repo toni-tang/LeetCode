8d2fa616e7ae49e4947f0fef4c4a06c04fb8efda
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-10-27 16:50:23 -0500

    Time: 3 ms (89.75%), Space: 14.2 MB (14.37%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        int res = nums[0];
-        vector<pair<int, int>> dp(nums.size());
-        
-        dp[0] = make_pair(nums[0], nums[0]);
+        int res = nums[0], pos = nums[0], neg = nums[0];
         
         for(int i = 1; i < n; i++) {
-            int pos = max(max(dp[i-1].first * nums[i], dp[i-1].second * nums[i]), nums[i]);
-            int neg = pos;
-            if(min(min(dp[i-1].first * nums[i], dp[i-1].second * nums[i]), nums[i]) < 0) {
-                neg = min(min(dp[i-1].first * nums[i], dp[i-1].second * nums[i]), nums[i]);
-            }
+            int tmp = pos;
+            pos = max({ pos * nums[i], neg * nums[i], nums[i] });
+            neg = min({ tmp * nums[i], neg * nums[i], nums[i] });
             
-            dp[i] = make_pair(pos, neg);
-            res = max(res, max(dp[i].first, dp[i].second));
+            res = max({res, pos, neg});
         }
         
         return res;
