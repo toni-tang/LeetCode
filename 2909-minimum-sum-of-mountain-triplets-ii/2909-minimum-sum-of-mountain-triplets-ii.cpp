0628701d706ac01751b1d9451d2a6541080199eb
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-10-27 14:02:49 -0500

    Time: 99 ms (98.49%), Space: 77.9 MB (92.31%) - LeetHub

--- 2909-minimum-sum-of-mountain-triplets-ii/2909-minimum-sum-of-mountain-triplets-ii.cpp
+++ 2909-minimum-sum-of-mountain-triplets-ii/2909-minimum-sum-of-mountain-triplets-ii.cpp
@@ -3,22 +3,21 @@ public:
     int minimumSum(vector<int>& nums) {
         vector<int> smallestRight(nums.size(), nums.back());
         
-        for(int i = nums.size()-2; i >= 0; i--){
+        for(int i = nums.size()-2; i >= 0; i--) {
             smallestRight[i] = min(smallestRight[i+1], nums[i]);
         }
         
-        int smallestLeft = nums[0], ans = -1;
+        int smallestLeft = nums[0], res = -1;
         
-        for(int i = 1; i < nums.size(); i++){
-            if(smallestLeft < nums[i] && smallestRight[i] < nums[i]){ 
-                if(ans == -1) ans =  nums[i] + smallestRight[i] + smallestLeft;
-                else ans = min(ans, nums[i] + smallestRight[i] + smallestLeft); 
+        for(int i = 1; i < nums.size()-1; i++) {
+            if(smallestLeft < nums[i] && smallestRight[i] < nums[i]) {
+                if(res != -1) res = min(res, smallestLeft + nums[i] + smallestRight[i]);
+                else res = smallestLeft + nums[i] + smallestRight[i];
             }
             
             smallestLeft = min(smallestLeft, nums[i]);
         }
         
-        return ans;
+        return res;
     }
-
 };
