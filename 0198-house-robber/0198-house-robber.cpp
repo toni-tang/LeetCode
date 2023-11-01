8097153f16ac8ba3bac81c5e8970a3045d0f0e62
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-01 17:47:58 -0500

    Time: 0 ms (100.00%), Space: 8.2 MB (29.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1);
-        
-        dp[1] = nums[0];
-        for(int i = 1; i < nums.size(); i++) {
-            dp[i+1] = max(dp[i-1] + nums[i], dp[i]);    
-        }
-        
-//         for(int n : dp) {
-//             cout << n << " ";
+// Recursive: 
+//     int rob(vector<int>& nums) {
+//         return solve(nums, nums.size()-1);
+//     }
+    
+//     int solve(vector<int>& nums, int i) {
+//         if(i < 0) {
+//             return 0;
 //         }
         
-//         cout << endl;
-        
-        return dp[nums.size()];
+//         return max(nums[i] + solve(nums, i-2), solve(nums, i-1));
+//     }
+
+// Memoization:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        solve(nums, n-1, dp);
+        return max(dp[n-1], dp[n-1]);
+    }
+    
+    int solve(vector<int>& nums, int i, vector<int>& dp) {
+        if(i < 0) {
+            return 0;
+        }
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = max(nums[i] + solve(nums, i-2, dp), solve(nums, i-1, dp));
     }
 };
