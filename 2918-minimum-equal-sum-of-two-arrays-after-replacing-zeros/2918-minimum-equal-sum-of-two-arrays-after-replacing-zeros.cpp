7837ddb82da1679a1f53ee8389921d20b43f0809
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-01 12:05:46 -0500

    Time: 168 ms (5.53%), Space: 130.6 MB (77.88%) - LeetHub

---
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        int zeroOne = 0, zeroTwo = 0;
+        long long sumOne = 0, sumTwo = 0;
+        
+        for(auto n : nums1) {
+            if(n == 0) zeroOne++;
+            sumOne += n;
+        }
+        
+        for(auto n : nums2) {
+            if(n == 0) zeroTwo++;
+            sumTwo += n;
+        }
+        
+        if(zeroOne == 0 && sumOne < sumTwo + zeroTwo) return -1;
+        if(zeroTwo == 0 && sumTwo < sumOne + zeroOne) return -1;
+        
+        return max(sumOne + zeroOne, sumTwo + zeroTwo);
+    }
+};
