504e2d66a48737b577d28c8632c52770a65ee4f8
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-04 17:19:38 -0500

    Time: 0 ms (100.00%), Space: 6.4 MB (87.14%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -29,20 +29,34 @@ public:
 //     }
     
 // Tabulation
+//     int numDecodings(string s) {
+//         int n = s.size();
+//         vector<int> dp(n+1, 0);
+//         dp[n] = 1;
+        
+//         for(int i = n-1; i >= 0; i--) {
+//             if(s[i] == '0') dp[i] = 0;
+//             else {
+//                 dp[i] = dp[i+1];
+//                 if(i < s.length()-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7'))) dp[i] += dp[i+2];
+//             }
+//         }
+        
+//         return dp[0];
+//     }
+    
+// Optimizied
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n+1, 0);
-        dp[n] = 1;
         
+        int curr = 0, pp = 0, p = 1;
         for(int i = n-1; i >= 0; i--) {
-            if(s[i] == '0') dp[i] = 0;
-            else {
-                dp[i] = dp[i+1];
-                if(i < s.length()-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7'))) dp[i] += dp[i+2];
-            }
+            curr = (s[i] == '0') ? 0 : p;
+            if(i < s.length()-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] < '7'))) curr += pp;
+            pp = p;
+            p = curr;
         }
         
-        return dp[0];
+        return curr;
     }
-    
 };
