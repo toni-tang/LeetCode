dfef3f01be8088d45baf54c3e41cf8137b90d596
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-04 21:35:08 -0500

    Time: 3 ms (36.44%), Space: 8.2 MB (31.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,59 +1,28 @@
 class Solution {
 public:
-// Recursive: 
+// Recursive:
 //     int rob(vector<int>& nums) {
-//         return solve(nums, nums.size()-1);
+//         return solve(nums, 0);
 //     }
     
 //     int solve(vector<int>& nums, int i) {
-//         if(i < 0) {
-//             return 0;
-//         }
+//         if(i == nums.size()-1 || i == nums.size()-2) return nums[i];
+//         if(i >= nums.size()) return 0;
         
-//         return max(nums[i] + solve(nums, i-2), solve(nums, i-1));
-//     }
-
-// Memoization (Top-Down):
-//     int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> dp(n, -1);
-//         solve(nums, n-1, dp);
-//         return max(dp[n-1], dp[n-1]);
+//         return max(nums[i] + solve(nums, i+2), solve(nums, i+1));
 //     }
     
-//     int solve(vector<int>& nums, int i, vector<int>& dp) {
-//         if(i < 0) {
-//             return 0;
-//         }
-//         if(dp[i] != -1) return dp[i];
-//         return dp[i] = max(nums[i] + solve(nums, i-2, dp), solve(nums, i-1, dp));
-//     }
-    
-// Tabulation (Bottom- Up):
-//     int rob(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> dp(n+1);
-        
-//         dp[1] = nums[0];
-//         for(int i = 1; i < n; i++) {
-//             dp[i+1] = max(dp[i-1] + nums[i], dp[i]);
-//         }
-        
-//         return dp[n];
-//     }
-    
-// Optimized:
+// Memoization: 
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return 0;
-        int dp1 = 0, dp2 = 0;
+        int n = nums.size();        
+        vector<int> dp(nums.size(), -1);
+        return solve(nums, 0, dp);
+    }
+    
+    int solve(vector<int>& nums, int i, vector<int>& dp) {
+        if(i >= nums.size()) return 0;
+        if(dp[i] > -1) return dp[i];
         
-        for(int i = 0; i < n; i++) {
-            int temp = dp1;
-            dp1 = max(nums[i] + dp2, dp1);
-            dp2 = temp;
-        }
-        
-        return max(dp1, dp2);
+        return dp[i] = max(nums[i] + solve(nums, i+2, dp), solve(nums, i+1, dp));
     }
 };
