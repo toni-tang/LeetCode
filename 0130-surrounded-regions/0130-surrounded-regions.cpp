96286fd64c160780c800b8e5ee60d5f1dc6b8caa
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-08 12:06:50 -0600

    Time: 9 ms (76.17%), Space: 10.2 MB (88.93%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -14,34 +14,25 @@ public:
             dfs(board, r-1, i);
         }
         
-        
         for(int i = 0; i < r; i++) {
             for(int j = 0; j < c; j++) {
-                if(board[i][j] == 'F') {
-                    board[i][j] = 'O';
-                }
-                else {
-                    if(board[i][j] == 'O') {
-                        board[i][j] = 'X';
-                    }
-                }
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                else if(board[i][j] == 'F') board[i][j] = 'O';
             }
         }
-        
     }
-private:
-    void dfs(vector<vector<char>>& board, int i, int j) {
-        if(i < 0 || j < 0 || i > board.size()-1 || j > board[0].size()-1) {
+    
+    void dfs(vector<vector<char>>& board, int r, int c) {
+        if(r < 0 || c < 0 || r >= board.size() || c >= board[0].size()) {
             return;
         }
         
-        if(board[i][j] != 'O') return;
+        if(board[r][c] != 'O') return;
+        if(board[r][c] == 'O') board[r][c] = 'F';
         
-        board[i][j] = 'F';
-        
-        dfs(board, i+1, j);
-        dfs(board, i-1, j);
-        dfs(board, i, j+1);
-        dfs(board, i, j-1);
+        dfs(board, r+1, c);
+        dfs(board, r-1, c);
+        dfs(board, r, c+1);
+        dfs(board, r, c-1);
     }
 };
