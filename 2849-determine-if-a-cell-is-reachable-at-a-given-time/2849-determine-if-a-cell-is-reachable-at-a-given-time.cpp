ade32eebdc840e564ebf7471a6a2587e34f767e8
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-08 11:40:23 -0600

    Time: 0 ms (100.00%), Space: 6.4 MB (5.30%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -3,6 +3,6 @@ public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
         int diffx = abs(fx - sx), diffy = abs(fy - sy);
         if(diffx == 0 && diffy == 0 && t == 1) return false;
-        return (min(diffx, diffy) + abs(diffx - diffy)) <= t;
+        return max(diffx, diffy) <= t;
     }
 };
