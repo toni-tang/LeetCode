50adfc56894d3f6b46b688f75e3db5e2ed11928e
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-08 12:20:21 -0600

    Time: 55 ms (67.50%), Space: 30.5 MB (48.11%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int r = grid.size(), c = grid[0].size();
+        
+        for(int i = 0; i < r; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, c-1);
+        }
+        
+        for(int i = 0; i < c; i++) {
+            dfs(grid, 0, i);
+            dfs(grid, r-1, i);
+        }
+        
         int res = 0;
-  
-        for(int i = 0; i < m; i++) {
-            if(grid[i][0] == 1) {
-                dfs(grid, i, 0);
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(grid[i][j] == 1) res += 1;
             }
-            if(grid[i][n-1] == 1) {
-                dfs(grid, i, n-1);
-            } 
         }
         
-        for(int j = 0; j < n; j++) {
-            if(grid[0][j] == 1) {
-                dfs(grid, 0, j);
-            }
-            if(grid[m-1][j] == 1) {
-                dfs(grid, m-1, j);
-            } 
-        }
-        
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 1) {
-                    res++;
-                }
-            }
-        }
-      
         return res;
     }
     
     void dfs(vector<vector<int>>& grid, int i, int j) {
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0) {
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != 1) {
             return;
         }
         
-        if(grid[i][j] == 1) grid[i][j] = 0;
+        if(grid[i][j] == 1) grid[i][j] = 2;
         
         dfs(grid, i+1, j);
-        dfs(grid, i, j+1);
         dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
         dfs(grid, i, j-1);
-        
-        return;
     }
 };
