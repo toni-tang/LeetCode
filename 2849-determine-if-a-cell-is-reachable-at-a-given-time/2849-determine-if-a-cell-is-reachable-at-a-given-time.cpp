6e113ca4782148577064e2501d89ad78ef2468b5
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-08 11:43:37 -0600

    Time: 0 ms (100.00%), Space: 6.5 MB (5.30%) - LeetHub

--- 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 2849-determine-if-a-cell-is-reachable-at-a-given-time/2849-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int diffx = abs(fx - sx), diffy = abs(fy - sy);
-        if(diffx == 0 && diffy == 0 && t == 1) return false;
-        return max(diffx, diffy) <= t;
+        int diffx = abs(fx - sx), diffy = abs(fy - sy); // Get distance between points
+        if(diffx == 0 && diffy == 0 && t == 1) return false; // Edge case
+        return max(diffx, diffy) <= t; // You can go diagonal therefore you only need to worry about the bigger of the two distances.
     }
 };
