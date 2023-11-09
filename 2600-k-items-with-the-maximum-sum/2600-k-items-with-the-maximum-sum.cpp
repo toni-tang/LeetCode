0842c5494a48c51b38fafdac50a7e897da98ca50
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-08 18:26:05 -0600

    Time: 0 ms (100.00%), Space: 6.5 MB (13.38%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        int res = 0;
-        while(k && numOnes) {
-            k--;
-            numOnes--;
-            res++;
-        }
-        while(k && numZeros){
-            k--;
-            numZeros--;
-        }
-        while(k && numNegOnes) {
-            k--;
-            numNegOnes--;
-            res--;
-        }
-        
-        return res;
+        return min(k, numOnes) - max(0, k - numOnes - numZeros);
     }
 };
