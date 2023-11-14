f434eeb29947c8d52d3ec721f87a3f5ad109b111
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-14 09:08:09 -0600

    Time: 444 ms (39.50%), Space: 15.1 MB (25.34%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int res = 0;
+        int n = s.size();
+        
+        unordered_map<char, unordered_set<char>> mp;
+        
+        for(int i = 0; i <= n-3; i++) {
+            if(mp.find(s[i]) == mp.end()) {
+                mp[s[i]] = unordered_set<char>();
+                for(int j = n-1; j > i; j--) {
+                    if(s[i] == s[j]) {
+                        for(int k = i + 1; k < j; k++) {
+                            mp[s[i]].insert(s[k]);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        
+        for(auto [k, v] : mp) {
+            res += v.size();
+        }
+        
+        return res;
+    }
+};
