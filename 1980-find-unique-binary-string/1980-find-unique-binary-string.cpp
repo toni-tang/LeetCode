b0f7073ac5e6e8317833ec3e2f0753dc47995b18
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-15 22:24:59 -0600

    Time: 3 ms (74.62%), Space: 10.4 MB (98.79%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,36 +1,13 @@
 class Solution {
 public:
-    
-    bitset<16> addOne(bitset<16>& binaryNumber) {
-        for (size_t i = 0; i < binaryNumber.size(); ++i) {
-            if (!binaryNumber[i]) {
-                binaryNumber[i] = 1;
-                break;
-            } else {
-                binaryNumber[i] = 0;
-            }
-        }
-        return binaryNumber;
-    }
-    
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
-        unordered_map<string, int> mp;
         
-        for(auto& str : nums) {
-            mp[str]++;
+        string res = "";
+        for(int i = 0; i < n; i++) {
+           res += nums[i][i] == '0' ? "1" : "0"; 
         }
         
-        bitset<16> bits;
-        for(int i = 0; i <= n*n; i++) {
-            string str = bits.to_string();
-            if(mp.find(str.substr(16 - n, 16)) == mp.end() ) {
-                return str.substr(16 - n, 16);
-            }
-               
-            addOne(bits);
-        }
-        
-        return nums[0][0] == '0' ? "1" : "0";
+        return res;
     }
 };
