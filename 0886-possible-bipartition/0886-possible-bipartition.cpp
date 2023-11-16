e93b6f6809c127f9f107379743bf7aeefc27bccb
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-16 17:03:50 -0600

    Time: 169 ms (49.68%), Space: 66.1 MB (55.66%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        const int RED = 1, WHITE = 0, BLUE = -1;
+        vector<vector<int>> adj(n+1, vector<int>());
+        
+        for(auto& relation : dislikes) {
+            adj[relation[0]].emplace_back(relation[1]);
+            adj[relation[1]].emplace_back(relation[0]);
+        }
+        
+        queue<int> q;
+        vector<int> color(n+1, 0);
+        vector<int> visited(n+1, 0);
+        
+        
+        for(int i = 1; i <= n; i++) {
+            if(!visited[i]) {
+               color[i] = RED;
+                q.push(i);
+            }
+            
+            while(!q.empty()) {
+                int curr = q.front(); q.pop();
+                
+                if(visited[curr]) continue;
+                
+                visited[curr] = 1;
+                
+                for(auto& node : adj[curr]) {
+                    if(color[curr] == color[node]) return false;
+                    
+                    if(color[curr] == RED) {
+                        color[node] = BLUE;
+                    }
+                    else {
+                        color[node] = RED;
+                    }
+                    
+                    q.push(node);
+                }
+            }
+        }
+        
+        return true;
+    }
+};
