da750dce2f2cf580913de66835ca3b895e4a5a9c
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-17 15:10:38 -0600

    Time: 199 ms (17.89%), Space: 96.6 MB (61.58%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -4,8 +4,8 @@ public:
         sort(nums.begin(), nums.end());
         
         int res = 0, n = nums.size();
-        for(int i = 0; i < n/2; i++) {
-            res = max(res, nums[i] + nums[n-i-1]);
+        for(int i = 0; i < n / 2; i++) {
+            res = max(res, nums[i] + nums[n - i - 1]);
         }
         
         return res;
