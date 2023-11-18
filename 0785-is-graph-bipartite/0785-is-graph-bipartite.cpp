b4cab36561f7e0878068c256b0d7008a075a723e
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-17 21:18:25 -0600

    Time: 19 ms (67.06%), Space: 14.2 MB (27.03%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -4,14 +4,7 @@ public:
         const int RED = 1, WHITE = 0, BLUE = -1;
         
         int n = graph.size();
-        vector<vector<int>> adj(n, vector<int>());
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < graph[i].size(); j++) {
-                adj[i].emplace_back(graph[i][j]);
-            }
-        }
-        
+
         queue<int> q;
         vector<int> color(n, 0);
         vector<int> visited(n, 0);
@@ -28,7 +21,7 @@ public:
                 if(visited[curr]) continue;
                 visited[curr] = 1;
                 
-                for(auto& neighbor : adj[curr]) {
+                for(auto& neighbor : graph[curr]) {
                     if(color[curr] == color[neighbor]) return false;
                     
                     color[neighbor] = (color[curr] == RED) ? BLUE : RED;
