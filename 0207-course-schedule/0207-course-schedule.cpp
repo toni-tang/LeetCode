f2ed5d1ceceb9bb057101a9a1cd7416356ce0bed
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-19 11:06:18 -0600

    Time: 15 ms (82.37%), Space: 13.7 MB (80.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,64 +1,36 @@
-// class Solution {
-// public:
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//         vector<bool> todo(numCourses, false), done(numCourses, false);
-//         vector<vector<int>> adj(numCourses, vector<int>());
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> adj(numCourses, vector<int>());
+        vector<int> degrees(numCourses, 0);
         
-//         for(int i = 0; i < prerequisites.size(); i++) {
-//             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-//         }
-        
-//         for (int i = 0; i < numCourses; i++) {
-//             if (!done[i] && !acyclic(adj, todo, done, i)) {
-//                 return false;
-//             }
-//         }
-        
-//         return true;
-//     }
-    
-// private:
-//     bool acyclic(vector<vector<int>> adj, vector<bool>& todo, vector<bool>& done, int node) {
-//         if (todo[node]) return false;
-//         if (done[node]) return true;
-        
-//         todo[node] = done[node] = true;
-
-//         for (int n : adj[node]) {
-//             if (!acyclic(adj, todo, done, n)) {
-//                 return false;
-//             }
-//         }
-        
-//         todo[node] = false;
-        
-//         return true;
-//     }
-// };
-
-class Solution  {
-public: 
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(n, vector<int>());
-        vector<int> degree(n, 0);
-
-        for (auto &p: pre) {
-            adj[p[0]].push_back(p[1]);
-            degree[p[1]]++;
+        for(auto& edge : prerequisites) {
+            adj[edge[1]].push_back(edge[0]);
+            degrees[edge[0]]++;
         }
-
+        
         queue<int> q;
-        for (int i = 0; i < n; i++) {
-            if (degree[i] == 0) q.push(i);
-        }
-
-        while (!q.empty()) {
-            int curr = q.front(); q.pop(); n--;
-            for (auto next: adj[curr]) {
-                if (--degree[next] == 0) q.push(next);   
+        for(int i = 0; i < numCourses; i++) {
+            cout << degrees[i] << " ";
+            if(degrees[i] == 0) {
+                q.push(i);
+                n--;
             }
         }
-
+        
+        while(!q.empty()) {
+            int curr = q.front(); q.pop();
+            
+            for(auto& neighbor : adj[curr]) {
+                degrees[neighbor]--;
+                if(degrees[neighbor] == 0) {
+                    q.push(neighbor);
+                    n--;
+                }
+            }
+        }
+        
         return n == 0;
-    }    
+    }
 };
