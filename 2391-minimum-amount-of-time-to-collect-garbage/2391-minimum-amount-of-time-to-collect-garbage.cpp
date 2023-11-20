6260a2961b53b0d2c7a290662fca09e784f8bc61
Tony Tang <50557412+toni-tang@users.noreply.github.com>
2023-11-20 11:06:25 -0600

    Time: 172 ms (49.51%), Space: 102.4 MB (91.88%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -3,29 +3,32 @@ public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         int res = 0;
         int n = garbage.size();
-        vector<pair<int, int>> countLastSeen(3, {0, 0});
+        vector<int> lastSeen(3, 0);
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < garbage[i].size(); j++) {
                 switch(garbage[i][j]) {
                     case 'G':
-                        countLastSeen[0] = {i, countLastSeen[0].second+1};
+                        lastSeen[0] = i;
                         break;
                     case 'P':
-                        countLastSeen[1] = {i, countLastSeen[1].second+1};
+                        lastSeen[1] = i;
                         break;
                      case 'M':
-                        countLastSeen[2] = {i, countLastSeen[2].second+1};
+                        lastSeen[2] = i;
                         break;
                 }
+                res++;
             }
         }
         
+        for(int i = 1; i < travel.size(); i++) {
+            travel[i] += travel[i-1];
+        }
+        
+        
         for(int i = 0; i < 3; i++) {
-            res += countLastSeen[i].second;
-            for(int j = 0; j < countLastSeen[i].first; j++) {
-                res += travel[j];
-            }
+            if(lastSeen[i] > 0) res += travel[lastSeen[i]-1];
         }
         
         return res;
